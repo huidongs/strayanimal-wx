ed338f0a92960ef62a4d562debb2cb710cc868a8
huidongs <2804234243@qq.com>
2020-11-27 08:37:42 +0800

    commit

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,9 @@
 // pages/tabbar/tabbar.js
 //获取应用实例
+var api = require('../../config/api.js');
+var util = require('../../utils/util.js');
+var user = require('../../utils/user.js');
+
 const app = getApp()
 
 Page({
@@ -8,37 +12,83 @@ Page({
    * 页面的初始数据
    */
   data: {
-    // userInfo : {},
-    // hasUserInfo: false,
-    // canIUse: wx.canIUse('button.open-type.getUserInfo')
+    userInfo: {
+      nickName: '点击登录',
+      avatarUrl: '/images/icon/my.png'
+    },
+    hasLogin: false,
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function () {
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
   },
- /* 我的收藏点击事件 */
-onMyCollection:function(){
-  wx.showModal({
-    title: '我的收藏',
-    content:  "收藏功能暂未开放 :(",
-    // success(res) {
-    //   if (res.confirm) {
-    //     console.log('用户点击确定')
-    //   } else if (res.cancel) {
-    //     console.log('用户点击取消')
-    //   }
-    //}
-  })
-},
-/* 是我订单点击事件 */
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    //获取用户的登录信息
+    if (app.globalData.hasLogin) {
+      let userInfo = wx.getStorageSync('userInfo');
+      this.setData({
+        userInfo: userInfo,
+        hasLogin: true
+      });
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+  goLogin: function () {
+    if (!this.data.hasLogin) {
+      wx.navigateTo({
+        url: "/pages/auth/login/login"
+      });
+    }
+  },
+  /* 我的收藏点击事件 */
+  onMyCollection: function () {
+    wx.showModal({
+      title: '我的收藏',
+      content: "收藏功能暂未开放 :(",
+      // success(res) {
+      //   if (res.confirm) {
+      //     console.log('用户点击确定')
+      //   } else if (res.cancel) {
+      //     console.log('用户点击取消')
+      //   }
+      //}
+    })
+  },
+  /* 是我订单点击事件 */
   onMyOrder: function (event) {
-   wx.navigateTo({
-     url: 'myorders/myorders'
-   })
+    wx.navigateTo({
+      url: 'myorders/myorders'
+    })
   },
   /* 意见反馈点击事件 */
   onSuggestion: function () {
@@ -68,58 +118,25 @@ onMyCollection:function(){
       //}
     })
   },
-/*  */
-  // toLogin:function(){
-  //   wx.navigateTo({
-  //     url: 'login/login',
-  //   })
-  // },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
---
+++ utils/user.js
@@ -0,0 +1,96 @@
+/**
+ * 用户相关服务
+ */
+const util = require('../utils/util.js');
+const api = require('../config/api.js');
+
+
+/**
+ * Promise封装wx.checkSession
+ */
+function checkSession() {
+  return new Promise(function(resolve, reject) {
+    wx.checkSession({
+      success: function() {
+        resolve(true);
+      },
+      fail: function() {
+        reject(false);
+      }
+    })
+  });
+}
+
+/**
+ * Promise封装wx.login
+ */
+function login() {
+  return new Promise(function(resolve, reject) {
+    wx.login({
+      success: function(res) {
+        if (res.code) {
+          resolve(res);
+        } else {
+          reject(res);
+        }
+      },
+      fail: function(err) {
+        reject(err);
+      }
+    });
+  });
+}
+
+/**
+ * 调用微信登录
+ */
+function loginByWeixin(userInfo) {
+
+  return new Promise(function(resolve, reject) {
+    return login().then((res) => {
+      //登录远程服务器
+      util.request(api.AuthLoginByWeixin, {
+        code: res.code,
+        avatarUrl: userInfo.avatarUrl,
+        wxNickname:userInfo.nickName
+      }, 'POST').then(res => {
+        if (res) {
+          //存储用户信息
+          console.log("这里是服务器返回的数据:"+res+"=="+res.data)
+          wx.setStorageSync('userInfo', res.data.user.userInfo);
+          wx.setStorageSync('token', res.data.token.token);
+
+          resolve(res);
+        } else {
+          reject(res);
+        }
+      }).catch((err) => {
+        reject(err);
+      });
+    }).catch((err) => {
+      reject(err);
+    })
+  });
+}
+
+/**
+ * 判断用户是否登录
+ */
+function checkLogin() {
+  return new Promise(function(resolve, reject) {
+    if (wx.getStorageSync('userInfo') && wx.getStorageSync('token')) {
+      checkSession().then(() => {
+        resolve(true);
+      }).catch(() => {
+        reject(false);
+      });
+    } else {
+      reject(false);
+    }
+  });
+}
+
+module.exports = {
+  loginByWeixin,
+  checkLogin,
+};
